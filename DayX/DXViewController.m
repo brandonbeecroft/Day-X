53b3a15f2ca95fecececd99a5a3411900c5ba5ca
Brandon Beecroft <brandonbeecroft@gmail.com>
2014-09-18 18:07:48 -0600

    as nsuserdefaults to the fields we have created so that data can be persisted

--- DayX/DXViewController.m
+++ DayX/DXViewController.m
@@ -8,10 +8,14 @@
 
 #import "DXViewController.h"
 
+static NSString * const appKey = @"appKey";
+static NSString * const appTitleKey = @"appTitleKey";
+static NSString * const appIdeaKey = @"appIdeaKey";
+
 @interface DXViewController ()
 
-@property (nonatomic, weak) IBOutlet UITextField *textField;
-@property (nonatomic, weak) IBOutlet UITextView *textView;
+@property (nonatomic, weak) IBOutlet UITextField *appTitle;
+@property (nonatomic, weak) IBOutlet UITextView *appIdeaTextView;
 @property (nonatomic, weak) IBOutlet UIButton *clearButton;
 
 @end
@@ -23,7 +27,16 @@
     // Do any additional setup after loading the view from its nib.
 
     self.title = @"Day X";
-    self.textField.delegate = self;
+    self.appTitle.delegate = self;
+    self.appIdeaTextView.delegate = self;
+
+    UIBarButtonItem *saveButton = [[UIBarButtonItem alloc] initWithTitle:@"Save" style:UIBarButtonItemStylePlain target:self action:@selector(saveEntry)];
+    [self.navigationItem setRightBarButtonItem:saveButton];
+    UIBarButtonItem *clearButton = [[UIBarButtonItem alloc] initWithTitle:@"Clear" style:UIBarButtonItemStylePlain target:self action:@selector(clear)];
+    [self.navigationItem setLeftBarButtonItem:clearButton];
+
+    NSDictionary *app = [[NSUserDefaults standardUserDefaults] objectForKey:appKey];
+    [self updateAppDictionary:app];
 
 }
 
@@ -37,9 +50,37 @@
     return YES;
 }
 
-- (IBAction)clear:(id)sender {
-    self.textField.text = @"";
-    self.textView.text = @"";
+- (IBAction)clear {
+    // set an alert view to make sure they want to clear
+    self.appTitle.text = @"";
+    self.appIdeaTextView.text = @"";
+}
+
+-(void)saveEntry {
+    NSLog(@"Entry Saved method");
+    [self.appIdeaTextView resignFirstResponder];
+
+    NSMutableDictionary *app = [NSMutableDictionary new];
+    if (self.appTitle.text) {
+        [app setObject:self.appTitle.text forKey:appTitleKey];
+    }
+    if (self.appIdeaTextView.text) {
+        [app setObject:self.appIdeaTextView.text forKey:appIdeaKey];
+    }
+
+    [[NSUserDefaults standardUserDefaults] setObject:app forKey:appKey];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+}
+
+-(void)updateAppDictionary:(NSDictionary *)dictionary {
+    NSString *appTitle = dictionary[appTitleKey];
+    NSString *appText = dictionary[appIdeaKey];
+
+    if (appTitle && appText) {
+        self.appTitle.text = appTitle;
+        self.appIdeaTextView.text = appText;
+    }
+
 }
 
 @end
